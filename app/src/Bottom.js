2637d81b9d3f548b4b0374f0467aff611f90c715
andre-ramos-rodrigues <andreramos@id.uff.br>
2024-08-26 16:45:08 -0300

    width

--- app/src/Bottom.js
+++ app/src/Bottom.js
@@ -8,7 +8,7 @@ const Bottom = () => {
      <div className='w-[100%] h-[100%] flex flex-col justify-center align-center items-center'>
      <div className='w-[100%] h-[100%] flex flex-col justify-center align-center items-center'>
      <p className='text-[22px] font-bold'>Lidar com o mal-estar abre espaço para vislumbrar aquilo que se deseja na vida</p> 
-     <p className='text-[16px] mt-4'>A terapia psicanalítica visa ajudar o sujeito a lidar com seu mal-estar, a partir do encontro com o inconsciente, habilitando-o a fazer escolhas diferentes daquelas que o adoeceram e a favor das que mobilizam seu desejo de viver.</p>
+     <p className='text-[16px] mt-4 max-w-[70%]'>A terapia psicanalítica visa ajudar o sujeito a lidar com seu mal-estar, a partir do encontro com o inconsciente, habilitando-o a fazer escolhas diferentes daquelas que o adoeceram e a favor das que mobilizam seu desejo de viver.</p>
      {/* <p className='text-[16px] '>construir bem feito para não ficar uma merda e utilizar os conceitos de forma coerente e apropriada para pessoas que não conhecem.</p> */}
      {/* <Link href="/candidatos" className='mt-2 text-[16px]'>AGENDA UMA SESSÃO</Link> */}
      </div>
