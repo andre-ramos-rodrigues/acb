a64adacd6bc5dda97b2249447444bfdf0e003409
andre-ramos-rodrigues <andreramos@id.uff.br>
2024-08-27 09:07:28 -0300

    centering text

--- app/src/Sinais.js
+++ app/src/Sinais.js
@@ -3,17 +3,19 @@ import { PiCircleFill } from "react-icons/pi";
 
 const Sinais = () =>  {
     return (
-      <div className=' bg-emerald-500 w-[100%] min-h-[300px] p-7 text-neutral-700 flex justify-center align-center items-center'>
+      <div className=' bg-teal-600 w-[100%] min-h-[300px] p-7 text-neutral-200 flex justify-center align-center items-center'>
        <div className='w-[100%] h-[100%] flex flex-col justify-center align-center items-center'>
        <div className='w-[100%] h-[100%] flex flex-col justify-center align-center items-center'>
        <p className='text-[22px] font-bold'>O mal-estar pode aparecer de várias formas</p> 
-       <div className='mt-4 flex flex-row gap-4 justify-center align-center items-center self-center'><PiCircleFill className='text-[10px]'/><p className='text-[20px]'>Depressão</p></div>
-       <div className='flex flex-row gap-4 justify-center align-center items-center self-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Ansiedade</p></div>
-       <div className='flex flex-row gap-4 justify-center align-center items-center self-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Fobias/Medos</p></div>
-       <div className='flex flex-row gap-4 justify-center align-center items-center self-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Procrastinação</p></div>
-       <div className='flex flex-row gap-4 justify-center align-center items-center self-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Autocobrança</p></div>
-       <div className='flex flex-row gap-4 justify-center align-center items-center self-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Outros</p></div>
+       <div className='flex flex-col place-content-start'>
+       <div className='mt-4 flex flex-row gap-4 items-center'><PiCircleFill className='text-[10px]'/><p className='text-[20px]'>Depressão</p></div>
+       <div className='flex flex-row gap-4 items-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Ansiedade</p></div>
+       <div className='flex flex-row gap-4 items-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Fobias/Medos</p></div>
+       <div className='flex flex-row gap-4 items-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Procrastinação</p></div>
+       <div className='flex flex-row gap-4 items-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Autocobrança</p></div>
+       <div className='flex flex-row gap-4 items-center'><PiCircleFill className='text-[10px]' /><p className='text-[20px]'>Outros</p></div>
        {/* <Link href="/candidatos" className='mt-2 text-[16px]'>AGENDA UMA SESSÃO</Link> */}
+       </div>
        </div>
        <div>
         {/* 
